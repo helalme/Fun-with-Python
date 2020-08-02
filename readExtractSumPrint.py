39def0c0fbbf88ec7d4925e0e78c856955b24806
Helal Chowdhury <helalme@users.noreply.github.com>
2020-08-02 12:27:23 +0200

    Read, extract, sum and print in 1 line

--- readExtractSumPrint.py
+++ readExtractSumPrint.py
@@ -1,3 +1,5 @@
+# Read a web text file, extracts integers, sum and print with a single line
+
 import re, requests
 
 print(sum([int(i) for i in re.findall('[0-9]+',requests.get("http://py4e-data.dr-chuck.net/regex_sum_42.txt").text)]))
